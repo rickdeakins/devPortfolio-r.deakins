76359fac2392f6138a196717a1f185aa1ea4b171
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-02 12:33:01 -0500

    Revisions made on tab header names to coincide with className fields defined in (currentPage ===).

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,9 @@ function Nav() {
           to="/"
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === '/About' ? 'nav-link active' : 'nav-link'}
         >
-          Home
+          About
         </Link>
       </li>
       <li className="nav-item">
@@ -32,7 +32,7 @@ function Nav() {
           // Check to see if the currentPage is `Resume`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/Resume' ? 'nav-link active' : 'nav-link'}
         >
-          About
+          Resume
         </Link>
       </li>
       <li className="nav-item">
@@ -41,7 +41,7 @@ function Nav() {
           // Check to see if the currentPage is `Portfolio`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === '/Portfolio' ? 'nav-link active' : 'nav-link'}
         >
-          Blog
+          Portfolio
         </Link>
       </li>
       <li className="nav-item">
