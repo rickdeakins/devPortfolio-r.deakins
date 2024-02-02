a400ff2fce0459bc919357eee1bce20ac73cffb7
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-02 10:00:00 -0500

    Navbar import updated with revised eport default label

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // Bringing in the required import from 'react-router-dom'
 import { Outlet } from 'react-router-dom';
-import Nav from './components/NavTabs';
+import Nav from './components/Navbar';
 
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
