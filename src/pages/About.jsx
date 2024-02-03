b9e43378d709494d1f2c733ddb0d27cff2f92479
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-03 15:53:10 -0500

    Initial style recorded

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,14 +1,15 @@
 export default function About() {
     return (
-      <div>
-        <h1>Rick Deakins</h1>
-        <p>
+      <div className="bg-light container-fluid">
+        <h1 className="fw-bold text-center text-medium pt-5">About Me</h1>
+        <p className="container text-justify col-4 bg-light pb-5">
           Greetings! My name is Rick Deakins. I am a web developer with a working background that delves into
           the creative as well as the technnical. With a background rooted in graphic design I have the ability 
-          to utilize design elements visually that completements my experience working in a more data-driven
+          to utilize visual design elements completementary to my experience working in a more data-driven
           environment managing reporting, databases, process design and client facing business operations. With 
           the utilization of logic and analytical thinking paired with a sound visual backdrop, the developmental
           outcome is a well-rounded final product. </p>
+          <img src="images/rick.jpg" alt="rick" width="400" height="auto" style={{ borderRadius: '10%' }} class="pb-5"/> 
       </div>
     );
-  }
+  } 
