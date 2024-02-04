8928759152d5953939654573d7911f6e2b06c965
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-04 01:48:34 -0500

    Contact form fields added to page. Not currently working. Sized to fit and scale correctly. Pending active connection.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,12 +3,24 @@ export default function Contact() {
       <div className="bg-light container-fluid" style={{ paddingTop: '170px' }}>
         <img src="images/contact.svg" alt="greetings" width="50%" className="pb-3"/> 
         <div className="container d-flex justify-content-start align-items-center pb-3">
-          <p className="col-lg-5 text-start mx-auto bg-light">
-          Contact Form
-          Contact Info
-          Links to Github, Linked In, etc.
-          </p><br/>
-        </div>
-      </div>
+          <p className="col-lg-5 text-center mx-auto bg-light">
+          <h3><strong>Contact Me With Any Inquiries</strong></h3>
+          <form style={{ marginBottom: '20%' }}>
+          <div class="mb-3">
+            <label for="Full Name" class="form-label">Full Name</label>
+            <input type="password" class="form-control" id="fullNameInput"/>
+          </div>  
+          <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+          </div>
+          <div className="mb-3">
+              <label htmlFor="messageInput" className="form-label">Message</label>
+              <textarea className="form-control" id="messageInput" rows="5"></textarea>
+              </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+        </p><br/>
+        </div></div>
     );
   }
