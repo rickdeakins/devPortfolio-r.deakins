ea4f176c2eeca9b0ed2642a70fd4e9765107aab7
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-04 01:17:55 -0500

    Applied UI elements initiated within About file. In place for content to be added to page.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,10 +1,12 @@
 export default function Portfolio() {
     return (
-      <div>
-        <h1>Portfolio</h1>
-        <p>
-         Portfolio works here
-        </p>
+        <div className="bg-light container-fluid" style={{ paddingTop: '170px' }}>
+        <img src="images/portfolio.svg" alt="greetings" width="50%" className="pb-3"/> 
+        <div className="container d-flex justify-content-start align-items-center pb-3">
+          <p className="col-lg-5 text-start mx-auto bg-light">
+            Portfolio data 
+          </p><br/>
+        </div>
       </div>
     );
   }
