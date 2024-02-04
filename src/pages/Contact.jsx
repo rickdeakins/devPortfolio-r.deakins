b227fe794e6aa8c7fec4a97c920416c4b07e0bb2
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-04 01:20:12 -0500

    Applied UI elements initiated within About file. In place for content to be added to page.

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,14 @@
 export default function Contact() {
     return (
-      <div>
-        <h1>Contact Page</h1>
-        <p>
+      <div className="bg-light container-fluid" style={{ paddingTop: '170px' }}>
+        <img src="images/contact.svg" alt="greetings" width="50%" className="pb-3"/> 
+        <div className="container d-flex justify-content-start align-items-center pb-3">
+          <p className="col-lg-5 text-start mx-auto bg-light">
           Contact Form
           Contact Info
           Links to Github, Linked In, etc.
-        </p>
+          </p><br/>
+        </div>
       </div>
     );
   }
