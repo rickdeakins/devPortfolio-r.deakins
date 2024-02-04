fba474ef2c8b26639db349e331c4f6ed8db61ca0
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-04 01:16:44 -0500

    Applied UI elements initiated within About file. In place for content to be added to pagre.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,10 +1,12 @@
 export default function Resume() {
     return (
-      <div>
-        <h1>Resume</h1>
-        <p>
-          Resume Image
-        </p>
+      <div className="bg-light container-fluid" style={{ paddingTop: '170px' }}>
+      <img src="images/resume.svg" alt="greetings" width="50%" className="pb-3"/> 
+      <div className="container d-flex justify-content-start align-items-center pb-3">
+        <p className="col-lg-5 text-start mx-auto bg-light">
+          Resume image here 
+        </p><br/>
       </div>
-    );
+    </div>
+    )
   }
