54f16ab068e0d0f8fbdb121fe627744fae4e7380
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-04 20:18:06 -0500

    Import line added - pending final copy of resume to be added to 'files' directory. Code created to export image of PDF in addiion to the download button. Pending resume pdf.

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,12 +1,13 @@
+  // import Resume from '/files/resume.pdf';
+
 export default function Resume() {
     return (
       <div className="bg-light container-fluid" style={{ paddingTop: '170px' }}>
-      <img src="images/resume.svg" alt="greetings" width="50%" className="pb-3"/> 
-      <div className="container d-flex justify-content-start align-items-center pb-3">
-        <p className="col-lg-5 text-start mx-auto bg-light">
-          Resume image here 
-        </p><br/>
+        <img src="images/resume.svg" alt="greetings" width="50%" className="pb-3"/> 
+      <br/>
+      <a href={Resume} download="Resume" target="_blank">
+        <button type="submit" class="btn btn-primary">Download Resume</button>  
+      </a>  
       </div>
-    </div>
     )
   }
