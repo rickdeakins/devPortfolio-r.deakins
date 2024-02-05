901df3c51861b1976dc522b0dd249b50487334af
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-05 16:48:31 -0500

    Portfolio structure set. Coming content updates for each project.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,12 +1,65 @@
-export default function Portfolio() {
-    return (
-        <div className="bg-light container-fluid" style={{ paddingTop: '170px' }}>
+  export default function Portfolio() {
+      return (
+        <div className="bg-light container-fluid align-items-center text-center" style={{ paddingTop: '170px' }}>
+          {/* Heading image */}
         <img src="images/portfolio.svg" alt="greetings" width="50%" className="pb-3"/> 
-        <div className="container d-flex justify-content-start align-items-center pb-3">
-          <p className="col-lg-5 text-start mx-auto bg-light">
-            Portfolio data 
-          </p><br/>
-        </div>
-      </div>
-    );
-  }
+        <div className="row">
+          <div className="w-50 mx-auto">
+            <div className="d-flex flex-column justify-content-center align-items-center mb-5">
+                {/* Content for the first flex box */}
+                <img src="images/passwordgenerator.png" alt="Image 1" className="img-fluid mb-2" />
+                <p>Password Generator Application</p>
+              </div> 
+            </div>
+        <div className="row">
+          <div className="w-50 mx-auto">
+            <div className="d-flex flex-column align-items-center">
+                {/* Content for the second flex box */}
+                <img src="images/regex.png" alt="Image 2" className="img-fluid mb-2" />
+                <p>Regex (Regular Expressions) Documentation</p>
+              </div>
+              </div>
+            </div>
+        <div className="row">
+          <div className="w-50 mx-auto">
+            <div className="d-flex flex-column align-items-center">
+                {/* Content for the third flex box */}
+                <img src="images/regex.png" alt="Image 2" className="img-fluid mb-2" />
+                <p>Regex (Regular Expressions) Documentation</p>
+              </div>
+              </div>
+            </div> 
+        <div className="row">
+          <div className="w-50 mx-auto">
+            <div className="d-flex flex-column align-items-center">
+                {/* Content for the fourth flex box */}
+                <img src="images/regex.png" alt="Image 2" className="img-fluid mb-2" />
+                <p>Regex (Regular Expressions) Documentation</p>
+              </div>
+              </div>
+            </div>
+        <div className="row">
+          <div className="w-50 mx-auto">
+            <div className="d-flex flex-column align-items-center">
+                {/* Content for the fifth flex box */}
+                <img src="images/regex.png" alt="Image 2" className="img-fluid mb-2" />
+                <p>Regex (Regular Expressions) Documentation</p>
+              </div>
+              </div>
+            </div>
+        <div className="row">
+          <div className="w-50 mx-auto">
+            <div className="d-flex flex-column align-items-cente" style={{marginBottom: "15%"}}>
+                {/* Content for the sixth flex box */}
+                <img src="images/regex.png" alt="Image 2" className="img-fluid mb-2" />
+                <p>Regex (Regular Expressions) Documentation</p>
+              </div>
+              </div>
+            </div>  
+
+
+            </div> 
+          </div>
+
+      );
+    }
