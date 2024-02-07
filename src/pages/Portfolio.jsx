c9b0cf899e4c6f85003dfccbd3428ded84ddc7f8
rickdeakins <rickdeakinsjr@gmail.com>
2024-02-07 11:45:55 -0500

    Added applicable deployment links to project images.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -7,7 +7,7 @@
           <div className="w-50 mx-auto">
             <div className="d-flex flex-column justify-content-center align-items-center mb-5">
                 {/* Content for the first flex box */}
-                <img src="images/passwordgenerator.png" alt="Image 1" className="img-fluid mb-2" />
+                <a href="https://rickdeakins.github.io/PasswordGeneratorgit"><img src="images/passwordgenerator.png" alt="Image 1" className="img-fluid mb-2" /></a>
                 <p><strong>Password Generator Application</strong><br/>
                 The sole purpose of this application is to generate a random password using a series of prompts. The user is initially prompted to choose a password length 
                 that is no less than eight characters and no longer than 128 characters.</p>
@@ -23,7 +23,6 @@
                 In this tutorial the user will be provided an in-depth review into regex (Regular Expression) 
                 for which an understanding at both the conceptual and technical levels can be reached.</p>
                 <a href="https://gist.github.com/rickdeakins/5d71d8abb7bd591d9fd5c0afa66cff77"><img src="images/github.svg" alt="github_logo" style={{ maxWidth: '4%', marginLeft: '10px' }} /></a>
-              
               </div>
               </div>
             </div>
@@ -43,7 +42,7 @@
           <div className="w-50 mx-auto">
             <div className="d-flex flex-column align-items-center">
                 {/* Content for the fourth flex box */}
-                <img src="images/workscheduler.png" alt="Image 2" className="img-fluid mb-2" />
+                <a href="https://rickdeakins.github.io/workScheduler/"><img src="images/workscheduler.png" alt="Image 2" className="img-fluid mb-2" /></a>
                 <p><strong>Work Scheduler Interactive Scheduling Application</strong><br/>
               A time tracking application to provide hourly timeblocks for the typical work day starting at 8:00am and ending at 5:00pm. 
               The text areas will dynamically update by color based on the current hour based on the past, present and future times. 
@@ -58,7 +57,7 @@
           <div className="w-50 mx-auto">
             <div className="d-flex flex-column align-items-center">
                 {/* Content for the fifth flex box */}
-                <img src="/images/NoteTaker.png" alt="Image 2" className="img-fluid mb-2" />
+                <a href="https://expressnotesv1-b059a69fb631.herokuapp.com"><img src="/images/NoteTaker.png" alt="Image 2" className="img-fluid mb-2" /></a>
                 <p><strong>Express Notes Note-Taking Application</strong><br/>
                 The Note Taker application is designed for the user to write notes, save their notes and review the saved notes at a later time. The user is prompted to add a title and their notes. 
                 To reviw the notes they have entered they simply just click on the note and it will be displayed on the right side of the screen.</p>
@@ -70,11 +69,11 @@
           <div className="w-50 mx-auto">
             <div className="d-flex flex-column align-items-cente" style={{marginBottom: "15%"}}>
                 {/* Content for the sixth flex box */}
-                <img src="/images/Text Editor.png" alt="Image 2" className="img-fluid mb-2" />
+                <a href="https://anotherwebtexteditor-8964003501f4.herokuapp.com"><img src="/images/Text Editor.png" alt="Image 2" className="img-fluid mb-2" /></a>
                 <p><strong>textEditorPWA</strong><br/>
                 This project is a Progessive Web Application Text Editor that is fully operational in the browser with 
                 capability to download as a Chrome Web Application to your local machine.</p>
-                <a href="https://github.com/rickdeakins/textEditorPWA"><img src="images/github.svg" alt="github_logo" style={{ maxWidth: '4%', marginLeft: '10px' }} /></a><br/>
+                <a href="https://github.com/rickdeakins/textEditorPWA"><img src="images/github.svg" alt="github_logo" style={{ maxWidth: '5%', marginLeft: '10px' }} /></a>
               </div>
               </div>
             </div>  
