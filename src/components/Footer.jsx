adbec0cc209d35afd466c052b1c42cf2d130c783
rickdeakins <rickdeakinsjr@gmail.com>
2026-05-05 00:15:24 -0400

    upd26files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const icon = {maxWidth: '2%', marginLeft: '10px'};
 const footer = {backgroundColor:"#ffff"}
 
     return(
-        <div id="footer" className="py-4 fixed-bottom" style={footer}>
+        <div id="footer" className="py-4" style={footer}>
       <a href="https://rickdeakins.netlify.app"><img src="images/rickdeakinslogoicon.svg" alt="github_logo" className="iconeffect" style={icon} /></a>
      
       <a href="https://www.linkedin.com/in/rick-deakins-14940315/"><img src="images/linkedin.svg" alt="linkedin_logo" className="iconeffect" style={icon} /></a> 
