b984bd8949a394a13d4230b4133e86e5282af79e
rickdeakins <rickdeakinsjr@gmail.com>
2026-05-05 00:38:26 -0400

    05

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,28 @@
-const containerStyle = {paddingTop: '15%', paddingBottom: '15%'}
-const contactText = {marginLeft: '50%',marginBottom: '50%'};
+
+
+const containerStyle = {
+  paddingTop: 'clamp(6rem, 6vw, 3rem)',
+  paddingBottom: 'clamp(1rem, 6vw, 3rem)',
+};
 
 export default function About() {
   return (
-<div className="bg-light container-fluid" style={containerStyle}>
-  <div className="row">
-    <div className="main-body">
-    <img src="images/bodytext_26.svg" alt="greetings" className="imagetext pb-9 align-items-center" />
-      <div className="image-container">
+    <div className="bg-light container-fluid about-root" style={containerStyle}>
+      <div className="row justify-content-center">
+        <div className="col-12 col-sm-10 col-md-8 main-body">
+          <img
+            src="images/bodytext_26.svg"
+            alt="greetings"
+            className="imagetext pb-3 mx-auto d-block"
+          />
+          <div className="image-container">
+            {/* other images/content here */}
+          </div>
+          <div className="imagetext align">
+            {/* text/content */}
+          </div>
         </div>
-       
-         <div className="imagetext align">
-      </div> 
-     </div>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
